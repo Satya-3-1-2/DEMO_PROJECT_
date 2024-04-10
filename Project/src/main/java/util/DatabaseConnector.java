bdf489afe7dadb1c44343f3a38a2f0874ac08d65
Satya Ranjan Barik <satyabarik8338@gmail.com>
2024-04-10 19:39:11 +0530

    small change

--- Project/src/main/java/util/DatabaseConnector.java
+++ Project/src/main/java/util/DatabaseConnector.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnector {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/user";
     private static final String JDBC_USER = "root";
-    private static final String JDBC_PASSWORD = "satya@2000rb";
+    private static final String JDBC_PASSWORD = "**********";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
